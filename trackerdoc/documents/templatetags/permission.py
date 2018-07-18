c1a35553406fcd6faf74f216ffb7a58fbb7ba27c
Amir Hamzah <2anaklelakilaut@gmail.com>
2018-07-19 00:42:46 +0800

    Added permission to edit or delete document

--- trackerdoc/documents/templatetags/permission.py
+++ trackerdoc/documents/templatetags/permission.py
@@ -13,3 +13,17 @@ def action_permission(event, user):
 def create_permission(user):
     groups = user.groups.all()
     return EventPermission.objects.filter(event__name="Baru", group__in=groups, allow=True).exists()
+
+@register.filter()
+def edit_permission(user):
+    groups = user.groups.all()
+    allow = EventPermission.objects.filter(event__name="Edit", group__in=groups, allow=True).exists()
+    print("Edit", allow)
+    return allow
+
+@register.filter()
+def delete_permission(user):
+    groups = user.groups.all()
+    allow = EventPermission.objects.filter(event__name="Delete", group__in=groups, allow=True).exists()
+    print("Delete:", allow)
+    return allow
